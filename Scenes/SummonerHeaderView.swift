9f8ab29c2fc9e26506817d283c03b96d2c1a4e27
Kang Byeonghak <hello@tinygram.dev>
2022-08-14 23:14:29 +0900

    SummonerHeaderView 연결

--- Scenes/SummonerHeaderView.swift
+++ Scenes/SummonerHeaderView.swift
@@ -9,4 +9,18 @@ import UIKit
 
 class SummonerHeaderView: UIView {
 
+  @IBOutlet weak var profileImageView: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var levelButton: UIButton!
+  @IBOutlet weak var refreshButton: UIButton!
+  
+  @IBOutlet weak var winLoseLabel: UILabel!
+  @IBOutlet weak var killDeathLabel: UILabel!
+  @IBOutlet weak var kdaLabel: UILabel!
+  
+  @IBOutlet var mostImageViews: [UIImageView]!
+  @IBOutlet var mostLabels: [UILabel]!
+  
+  @IBOutlet weak var positionImageView: UIImageView!
+  @IBOutlet weak var positionLabel: UILabel!
 }
