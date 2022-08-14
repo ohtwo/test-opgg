b2e633d4533119a17dfadf5003f26635c7cb44d1
Kang Byeonghak <hello@tinygram.dev>
2022-08-14 23:02:29 +0900

    SummonerGameCell 연결

--- Scenes/SummonerGameCell.swift
+++ Scenes/SummonerGameCell.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class SummonerGameCell: UITableViewCell {
 
+  @IBOutlet weak var resultView: UIView!
+  @IBOutlet weak var winLoseLabel: UILabel!
+  @IBOutlet weak var gameLengthLabel: UILabel!
+  
+  @IBOutlet weak var championImageView: UIImageView!
+  @IBOutlet weak var scoreBadge: UIButton!
+  @IBOutlet var spellImageViews: [UIImageView]!
+  @IBOutlet var itemImageViews: [UIImageView]!
+  
+  @IBOutlet weak var killDeathLabel: UILabel!
+  @IBOutlet weak var contributionLabel: UILabel!
+  
+  @IBOutlet weak var gameTypeLabel: UILabel!
+  @IBOutlet weak var createDateLabel: UILabel!
+  @IBOutlet weak var multiKillLabel: UILabel!
+  
   override func awakeFromNib() {
     super.awakeFromNib()
   }
